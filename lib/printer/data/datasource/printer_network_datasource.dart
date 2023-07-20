1effcde63fbb35f0b1486e4663e3174495f22499
sgsm74 <saeed.razii93@gmail.com>
2023-07-20 14:47:04 +0330

    chore: add html to pdf package

--- lib/printer/data/datasource/printer_network_datasource.dart
+++ lib/printer/data/datasource/printer_network_datasource.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:typed_data';
 
+import 'package:htmltopdfwidgets/htmltopdfwidgets.dart';
 import 'package:pdf/pdf.dart';
 import 'package:printing/printing.dart';
 
@@ -16,9 +17,29 @@ class PrinterNetworkDatasourceImp implements PrinterNetworkDatasource {
 
   @override
   Future<void> startUsbPrint(Printer printer, Uint8List pdf) async {
+    const htmlContent = '''
+  <h1>Heading Example</h1>
+  <p>This is a paragraph.</p>
+  <img src="image.jpg" alt="Example Image" />
+  <blockquote>This is a quote.</blockquote>
+  <ul>
+    <li>First item</li>
+    <li>Second item</li>
+    <li>Third item</li>
+  </ul>
+''';
+
+    final newpdf = Document();
+    List<Widget> widgets = await HTMLToPdf().convert(htmlContent);
+    newpdf.addPage(MultiPage(
+        maxPages: 200,
+        build: (context) {
+          return widgets;
+        }));
+    final r = await newpdf.save();
     await Printing.directPrintPdf(
       printer: printer,
-      onLayout: (layout) => pdf,
+      onLayout: (layout) => r,
       format: PdfPageFormat.roll80,
     );
   }
